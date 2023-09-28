b4042ad5f8d92d5938dad5d47dd2372d726fe9a3
joseluizs <joseluiz.smarques@gmail.com>
2023-09-28 16:31:53 -0300

    implementação da camada de domínio

---
+++ src/main/java/com/br/luiz/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package com.br.luiz.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+
+}
---
+++ src/main/java/com/br/luiz/domain/model/News.java
@@ -0,0 +1,9 @@
+package com.br.luiz.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News  extends BaseItem {
+
+
+}
